531a1a2ab31770d8626a65b24cc021bd68ba005d
Tousif Islam <tousifislam099@gmail.com>
2024-09-16 13:53:29 +0530

    Indigo.py

--- Indigo.py
+++ Indigo.py
@@ -1,13 +1,12 @@
 import webbrowser
 import pyttsx3
 import speech_recognition as sr
-import google.generativeai as genai
 import os
-
+import time
+import requests
 
 recognizer = sr.Recognizer()   
 engine = pyttsx3.init()
-
 
 
 
@@ -17,6 +16,10 @@ url2="https://www.facebook.com/"
 url3="https://www.instagram.com/"
 url4="https://chatgpt.com/"
 url5="https://www.pw.live/"
+
+        
+def speak_for_alarm():
+    speak(f"its the time......{current_time}")
 
 def speak(text):
     engine.say(text)
@@ -67,14 +70,42 @@ elif "open notepad" in command:
 elif "open calculator" in command:
         speak("Opening Calculator...")
         os.system("calc.exe")
-elif "shut down" in command:
+elif "shut down the system" in command:
     speak("shutting down the computer")
     os.system("shutdown /s /t 1")
-elif "restart" in command:
+elif "restart the system" in command:
     speak("restarting down the computer")
     os.system("restart /r /t 1")
+elif "set alarm" in command:
+    speak("enter the time for the alarm to ring")
+    alarm_time=input("enter the time for the alarm to ring: (HH:MM): ")
+    speak("the alarm is set")
+
+    while True:
+        current_time=time.strftime("%H:%M")
+        if current_time == alarm_time:
+            print(f"its the time......{current_time}")
+            speak_for_alarm()
+            speak_for_alarm()
+            speak_for_alarm()
+            break
+elif "weather information" in command:
+    speak("enter your city name")
+    city=input("enter your city name")
+    
+    url=f"https://wttr.in/{city}?format%t%20%w"
+
+    def get_weather(city):
+        response=requests.get(url)
+        try:
+            speak(f"the weather information for your city{city}is:")
+            print(f"the weather of {city} is:{response.text}")
+        except:
+            print("I couldn't find")
+
+    get_weather(city)
 else:
     speak("I didn't understand that. Searching the web for you...")
-    search_query = command  # Use the command as the search query
+    search_query = command
     search_url = f"https://www.google.com/search?q={search_query}"
     webbrowser.open(search_url)
